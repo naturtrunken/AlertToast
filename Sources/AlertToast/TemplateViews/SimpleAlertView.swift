3d479328e408640073c952af6155fe707573ddee
Andreas Marc Klingler <post@andreas-klingler.de>
2023-01-23 18:05:25 +0100

    a

--- Sources/AlertToast/TemplateViews/SimpleAlertView.swift
+++ Sources/AlertToast/TemplateViews/SimpleAlertView.swift
@@ -14,32 +14,35 @@ struct SimpleAlertView: View {
     var foregroundColor: Color
     
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                
-                Image(systemName: icon)
-                    .font(.custom("Sohne-Buch", size: 38.0))
-                    .frame(width: 20.0)
-                    .padding(.trailing)
-                Text(title)
-                    .multilineTextAlignment(.leading)
-                    .font(.custom("Sohne-Buch", size: 18.0))
-                    .frame(width: 270)
-                    .lineSpacing(/*@START_MENU_TOKEN@*/6.0/*@END_MENU_TOKEN@*/)
-                
-                Spacer()
+        Group {
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Image(systemName: icon)
+                        .font(.custom("Sohne-Buch", size: 38.0))
+                        .frame(width: 20.0)
+                        .padding(.trailing)
+                    Text(title)
+                        .multilineTextAlignment(.leading)
+                        .font(.custom("Sohne-Buch", size: 18.0))
+                        .frame(width: 270)
+                        .lineSpacing(/*@START_MENU_TOKEN@*/6.0/*@END_MENU_TOKEN@*/)
+                    
+                    Spacer()
+                }
+                .foregroundColor(foregroundColor)
+                .padding()
             }
-            .foregroundColor(foregroundColor)
-            .padding()
+            .frame(minHeight: 50)
+            .fixedSize(horizontal: true, vertical: false)
+            .alertBackground(backgroundColor)
+            .clipShape(Rectangle())
+            .overlay(Rectangle().stroke(Color.gray.opacity(0.2), lineWidth: 1))
+            .cornerRadius(28.0)
+            .shadow(color: Color.black.opacity(0.25), radius: 5, x: 0, y: 10)
         }
-        .frame(minHeight: 50)
-        .fixedSize(horizontal: true, vertical: false)
-        .alertBackground(backgroundColor)
-        .clipShape(Rectangle())
-        .overlay(Rectangle().stroke(Color.gray.opacity(0.2), lineWidth: 1))
-        .cornerRadius(28.0)
-        .shadow(color: Color.black.opacity(0.25), radius: 5, x: 0, y: 10)
+        .padding(.top, 100)
     }
 }
 
